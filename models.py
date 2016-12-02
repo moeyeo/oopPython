df8f21b180dc0a60f6528f060c44afe683303132
eyekanarat <you@example.com>
2016-12-02 23:08:27 +0700

    gold appear and add class Model

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
+import arcade.key
+
+class Model:
+    def __init__(self, world, x, y, angle):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.angle = 0
 
 class World:
     def __init__(self, width, height):
@@ -5,18 +13,43 @@ class World:
         self.height = height
  
         self.ship = Ship(self, 100, 100)
+        self.gold = Gold(self, 400, 400)
  
  
     def animate(self, delta):
         self.ship.animate(delta)
 
-class Ship:
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.SPACE:
+            self.ship.switch_direction()        
+
+class Ship(Model):
+    DIR_HORIZONTAL = 0
+    DIR_VERTICAL = 1
+ 
     def __init__(self, world, x, y):
-        self.world = world
-        self.x = x
-        self.y = y
+        super().__init__(world, x, y, 0)
+ 
+        self.direction = Ship.DIR_VERTICAL
+
+    def switch_direction(self):
+        if self.direction == Ship.DIR_HORIZONTAL:
+            self.direction = Ship.DIR_VERTICAL
+            self.angle = 0
+        else:
+            self.direction = Ship.DIR_HORIZONTAL
+            self.angle = -90
  
     def animate(self, delta):
-        if self.y > self.world.height:
-            self.y = 0
-        self.y += 5
+        if self.direction == Ship.DIR_VERTICAL:
+            if self.y > self.world.height:
+                self.y = 0
+            self.y += 5
+        else:
+            if self.x > self.world.width:
+                self.x = 0
+            self.x += 5
+
+class Gold(Model):
+    def __init__(self, world, x, y):
+        super().__init__(world, x, y, 0)
